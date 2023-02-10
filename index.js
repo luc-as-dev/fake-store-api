1f58e066e1bcc292a551711d8c40207e4d2f3426
Lucas Karlsson <lucas.karlsson@post.com>
2023-02-10 19:23:13 +0100

    added GET for /products /products/:id

--- index.js
+++ index.js
@@ -5,8 +5,27 @@ import cors from "cors";
 const PORT = 3000;
 
 const app = express();
+const products = [
+    {id:1,name:"Phone",price:8990},
+    {id:2,name:"Laptop",price:14990},
+    {id:3,name:"Headphones",price:1490}
+];
 
 app.use(cors({origin:"*"}));
+
+app.get("/products",(req,res)=>{
+    res.send(products);
+});
+
+app.get("/products/:id",(req,res)=>{
+    const product = products.find(p=>p.id==req.params.id);
+    if(!product) {
+        res.status(404).send();
+    } else {
+        res.send(product)
+    }
+
+});
 
 app.listen(PORT);
 console.log(`Started server on port: ${PORT}`);
